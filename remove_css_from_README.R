45a9cc45a0d1bbcf9c9240fad98bef3ec5540683
zheer-kejlberg <zheer-kejlberg@users.noreply.github.com>
2023-11-05 00:23:55 +0100

    updated the remove_css file for cleaner regex

--- remove_css_from_README.R
+++ remove_css_from_README.R
@@ -1,4 +1,4 @@
 tx  <- readLines("README.md")
 tx <- paste(tx, collapse="\n")
-tx  <- gsub(pattern = "<style>(\r\n|\r|\n|.)*?<\\/style>", replacement = "", x = tx)
+tx  <- gsub(pattern = "<style>(\r\n|\r|\n|.)*?<[/]style>", replacement = "", x = tx)
 writeLines(tx, con="README.md")
