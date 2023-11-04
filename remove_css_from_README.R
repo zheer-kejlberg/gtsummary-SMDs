b9fd472d2b63895491ae555849657c09a155ad5e
zheer-kejlberg <zheer-kejlberg@users.noreply.github.com>
2023-11-04 16:16:43 +0100

    Updated, now includes an R file to remove unnecessary CSS from the CSS output of Rmarkdown

--- remove_css_from_README.R
+++ remove_css_from_README.R
@@ -1,13 +1,5 @@
 library(stringr)
 tx  <- readLines("README.md")
-tx2 <- paste(tx, collapse="\n")
-
-str_count(tx2, "<style>(\r\n|\r|\n|.)*</style>")
-
-
-tx3  <- gsub(pattern = "<style>(\r\n|\r|\n|.)*</style>", replacement = " ", x = tx2)
-
-
-
-writeLines(tx3, con="README2.md")
-
+tx <- paste(tx, collapse="\n")
+tx  <- gsub(pattern = "<style>(\r\n|\r|\n|.)*?<\\/style>", replacement = "", x = tx)
+writeLines(tx, con="README.md")
